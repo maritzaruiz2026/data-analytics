f52eeb9e175d3b0f2fe22ad5bcc331f264b19a57
Maritza Ruiz <mruiz3@my.yearupunited.org>
2026-04-13 10:23:45 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database that contains information in regards of actors in movies, the film categories, the film rental information/inventory and the customers who rent these films. 
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+SELECT * FROM film; -- Retrieved 1000 records
