8427c3b071b0547cef162e3acaadaac89569b309
Maritza Ruiz <mruiz3@my.yearupunited.org>
2026-04-13 10:06:27 -0500

    Ex1B for Week-02

---
+++ week-02/Ex1B_first_query.sql
@@ -0,0 +1,3 @@
+/*
+Sakila is a database that contains information in regards of actors in movies, the film categories, the film rental information/inventory and the customers who rent these films. 
+*/
