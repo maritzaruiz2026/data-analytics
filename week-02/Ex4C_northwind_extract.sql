461dc634f7c052c11f6be9d6d3bd58fb3f16e53c
Maritza Ruiz <mruiz3@my.yearupunited.org>
2026-04-14 12:01:14 -0500

    Exercise 4.C Adding the Northwind Database

---
+++ week-02/Ex4C_northwind_extract.sql
@@ -0,0 +1,5 @@
+/* 
+a) The name of the table that holds the items Northwind sells is table products.
+b) The name of the table that holds the types/categories of the items Northwind sells is table categories.
+*/
+
