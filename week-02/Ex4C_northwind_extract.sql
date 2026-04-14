d2cd476e2ac07f07b2334ec96664f12e77c2b25d
Maritza Ruiz <mruiz3@my.yearupunited.org>
2026-04-14 12:05:03 -0500

    Exercise 4.C Adding the Northwind Database

--- week-02/Ex4C_northwind_extract.sql
+++ week-02/Ex4C_northwind_extract.sql
@@ -1,5 +1,6 @@
 /* 
-a) The name of the table that holds the items Northwind sells is table products.
-b) The name of the table that holds the types/categories of the items Northwind sells is table categories.
+4.a) The name of the table that holds the items Northwind sells is table products.
+4.b) The name of the table that holds the types/categories of the items Northwind sells is table categories.
 */
 
+SELECT * FROM employees; -- 5.a) The Northwind employee whose name makes it look like she's a bird is employee on row 4 (Employee ID 4) which is Peackock Margaret.
