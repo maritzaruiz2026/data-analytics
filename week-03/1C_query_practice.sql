acfa122885bb0b7cbf18543fe84fa16c9c6ecd59
Maritza Ruiz <mruiz3@my.yearupunited.org>
2026-04-22 23:18:34 -0500

    Exercise 1.C More practice with queries

---
+++ week-03/1C_query_practice.sql
@@ -0,0 +1,27 @@
+-- 1. Write a query to list the product id, product name, and unit price of every product. This time, display them in ascending order by price.
+USE northwind;
+
+-- 2. What are the products that we carry where we have at least 100 units on hand? Order them in descending order by price.
+USE northwind;
+
+/* 3. What are the products that we carry where we have at least 100 units on hand? Order them in descending order by price. 
+If two or more have the same price, list those in ascending order by product name. */
+USE northwind;
+
+/* 4. Write a query against the orders table that displays the total number of distinct customers who have placed orders, based on customer ID. 
+Use an alias to label the count calculation as CustomerCount. */
+USE northwind;
+
+/* 5. Write a query against the orders table that displays the total number of distinct customers who have placed orders, by customer ID, 
+for each country where orders have been shipped. Again, use an alias to label the count as CustomerCount. Order the list by the CustomerCount, largest to smallest. */
+USE northwind;
+
+/* 6. What are the products that we carry where we have less than 25 units on hand, but 1 or more units of them are on order? 
+Write a query that orders them by quantity on order (high to low), then by product name. */
+USE northwind;
+
+-- 7. Write a query to list each of the job titles in employees, along with a count of how many employees hold each job title.
+USE northwind;
+
+-- 8. What employees have a monthly salary that is between $2000 and $2500? Write a query that orders them by job title.
+USE northwind;
