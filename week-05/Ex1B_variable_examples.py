41fb1f24fcb3a9c49a35edb62c6f1bb2a1460d1a
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-06 10:30:00 -0500

    Exercise 1.B Getting started with Python

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,34 @@
+customer_id = 1596 # unique identifier for each customer
+customers_name = 'Mariana Ruiz' #Customer's name as a string
+customer_gender = "Female" #Gender as string
+customer_DOB = '1995-12-24' #Date of birth as a string
+dl_number = 'R45269825' #Driver's license number as a string
+auto_policy_number = 'PN-1151224'#Auto insurance policy number as a string
+my_name = 'Maritza Ruiz' #My name as a string
+city_state = 'Chicago, IL' #City and state as a string
+
+
+#LAB 2
+#What is the full list of reserved words that can’t be used for variable names?
+#False	^^def	if	^^raise
+#^^None	del	import	return
+#True	^^elif	in	^^try
+#and	else	^^is	^^while
+#as	except	^^lambda	^^with
+#^^assert	^^finally	^^nonlocal	^^yield
+#^^break	^^for	^^not
+#^^class	^^form	or
+#^^continue	^^global	^^pass
+
+#Pick 5 of these words and review the explanation for how it is used as a keyword in Python. Add these 5 definitions as # comments to your exercise document. Put ^^ around any terms that you are not familiar with.
+#1. elif:
+    #Used in conditional statements, it stands for "else if" and allows you to check multiple conditions after an initial if statement.
+#2. global: 
+    #Used to declare that a variable inside a function is global (i.e., it exists outside the function and can be accessed and modified globally).
+#3. lambda: 
+    #Used to create anonymous functions (functions without a name) in a single line of code.
+#4. break: 
+    #Used to exit a loop prematurely when a certain condition is met.
+#5. assert: 
+    #Used for debugging purposes, it tests if a condition is true and raises an AssertionError if it is not.
+#I am unfamiliar with all terms, the only ones I have some idea of what they mean are: False, if, import, return, True, in, and, else, as, except, with, del and or. 
