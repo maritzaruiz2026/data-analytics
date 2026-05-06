a057aa02e157dfab7a34a6a40df918a2d025af15
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-06 10:11:34 -0500

    Exercise 1.A Getting Ready for Week 5

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,15 @@
 print('Hello World')
+
+message = 'Hello World'
+print(message)
+
+#Display dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents) #The output was 3.5 and ignored the second zero after 5
+cents += .25
+print(dollars + cents) 
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + ' and ' + c_str) #The output was 3 dollars and 50 cents
