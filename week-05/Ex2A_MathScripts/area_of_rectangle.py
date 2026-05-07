43789dcca423a1390ea4cfeb1654e407181a103c
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-07 16:13:53 -0500

    Exercise 2.A Make calculations with Python LAB 2

--- week-05/Ex2A_MathScripts/area_of_rectangle.py
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -1,1 +1,14 @@
-#
+#The area of a rectangle is calculated by multiplying the length and width of the rectangle.
+# Area = Length * Width
+
+length = 12
+width = 24
+
+def rectangle_area(length, width):
+    area = length * width
+    return area
+
+area = rectangle_area(length, width)
+print(f"The area of the rectangle is {area}")
+print(f"Side A is {length}")
+print(f"Side B is {width}")
