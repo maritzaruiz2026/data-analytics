ff77b553fc81a6ef0e6b969490d49b0b9b4db528
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-08 14:58:10 -0500

    Exercise 2.A Make calculations with Python LAB 3

--- week-05/Ex2A_MathScripts/tip_amount.py
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -1,12 +1,21 @@
 #How do you calculate the tip amount on a restaurant bill given the tip percentage?
 #Tip Amount=Bill Amount*Tip Percentage
 
+
+#bill_amount = 73.20
+#tip_percentage = 15
+
+bill_amount = float(input("How much was your bill? ")) 
+print("Your bill amount is " + str(bill_amount))
+
+tip_percentage = float(input("What percentage would you like to tip? ")) 
+print("Your tip percentage is " + str(tip_percentage))
+
 def calculate_tip_amount(bill_amount, tip_percentage):
     tip_amount = bill_amount * (tip_percentage / 100)
     return tip_amount
 
-
-bill_amount = 73.20
-tip_percentage = 15
 tip_amount = calculate_tip_amount(bill_amount, tip_percentage)
 print(f"The tip on a ${bill_amount:.2f} restaurant bill is ${tip_amount:.2f}")
+
+# When I originally ran my script I got an error saying could not convert string into float. I had the def section in the beggining of the script and moved it to the middle and it worked!
