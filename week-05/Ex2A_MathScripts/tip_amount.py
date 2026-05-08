1434783645ecd2657072da5fb75fe4aeabf26fbf
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-08 12:52:38 -0500

    Exercise 2.A Make calculations with Python LAB 2

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,11 @@
+#How do you calculate the tip amount on a restaurant bill given the tip percentage?
+#Tip Amount=Bill Amount*Tip Percentage
+
+def calculate_tip_amount(bill_amount, tip_percentage):
+    tip_amount = bill_amount * (tip_percentage / 100)
+    return tip_amount
+
+bill_amount = 73.20
+tip_percentage = 15
+tip_amount = calculate_tip_amount(bill_amount, tip_percentage)
+print(f"The tip on a ${bill_amount:.2f} restaurant bill is ${tip_amount:.2f}")
