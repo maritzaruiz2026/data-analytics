298683fbb6e63d24e0bb56d0d679b29ef13d9d0b
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-09 22:37:19 -0500

    Exercise 3.A Numeric conversion exercises LAB 1

--- week-05/Ex3A_conversion_tests.py
+++ week-05/Ex3A_conversion_tests.py
@@ -1,20 +1,42 @@
-# Description: This script tests various numeric 
-#   conversion techniques 
-# Author: Sam Q. Newprogrammer
+# Description: This script tests various numeric conversion techniques 
+# Author: Maritza Ruiz
 
 
 a = " 101.1 " 
 b = '55' 
-c = "402 Stevens" 
+c = "402 Stevens"
 d = 'Number 5 '
 
-#print(int(a)) #This caused an error because of the decimal point and the spaces, says invalid literal for int() with base 10:' 101.1 '
-#print(float(a)) #This worked and converted the string to a float, ignoring the spaces. Output was 101.1
-#print(int(float(a))) #This worked and converted the string to an integer, ignoring the spaces and the decimal point. Output was 101
-#print(int(b)) #This worked and converted the string to an integer. Got 55 as output
-#print(float(b)) #This worked and converted the string to a float. Output was 55.0
-#print(int(c)) #This caused an error because of the non-numeric characters. Output was invalid literal for int() with base 10: '402 Stevens'
-#print(float(c)) #This caused an error because of the non-numeric characters. Output was invalid literal for float(): '402 Stevens' 
-#print(int(d)) #This caused an error because of the non-numeric characters. Output was invalid literal for int() with base 10: 'Number 5 '
-#print(float(d)) #This caused an error because of the non-numeric characters. Output was invalid literal for float(): 'Number 5 '
+print(a)
+a = ' 103.3 '
+print(a, type(a)) 
+#print(int(a)) #This caused an error because of the decimal point and the spaces, says invalid literal for int() with base 10:' 103.3 '
+print(float(a)) 
+print(int(float(a))) 
+print(a.strip()) 
 
+
+print(b)
+b = '65'
+print(b, type(b))
+#print(int(b)) #This caused an error because of the decimal point, says invalid literal for int() with base 10: '65'
+print(float(b)) 
+
+
+print(c)
+c = "710 Regan Street"
+print(c, type(c))   
+#print(int(c)) #This caused an error because of the non-numeric characters. Output was invalid literal for int() with base 10: "710 Regan Street"
+#print(float(c)) #This caused an error because of the non-numeric characters. Output was invalid literal for float(): "710 Regan Street"
+print(c) 
+print(c[0:3])
+
+
+print(d)
+d = 'Number 25 '
+print(d, type(d))   
+#print(int(d)) #This caused an error because of the non-numeric characters. Output was invalid literal for int() with base 10: 'Number 25 '
+#print(float(d)) #This caused an error because of the non-numeric characters. Output was invalid literal for float(): 'Number 25 '
+print(d) 
+print(d[7:9])
+print(d.strip()) 
