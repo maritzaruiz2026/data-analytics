aac1d76f5461c770c4ace8468d99ed6fc142abdd
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-11 06:39:21 -0500

    Exercise 4.C Loop de loop LAB 1

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,5 @@
+great = ['amazing', 'awesome', 'excellent', 'stupendous', 'wonderful', 'fantastic']
+for y in great:
+    print('Loops are ' + y + '!')
+    #break, originally had break and it would stop my loop after the first result
+print ('I <3 loops')
