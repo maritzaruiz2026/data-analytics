0f6db5de95ee7b8db11c85fabf170a5712b987d1
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-14 12:47:25 -0500

    Exercise 2.B Using library functions LAB #

---
+++ week-06/Ex2B_Functions/math_and_stats.py
@@ -0,0 +1,10 @@
+import random
+import math
+import statistics
+
+vals_1_100 = range(1,100) 
+vals_sample = random.sample(vals_1_100, 75) 
+vals_choices = random.choices(vals_1_100, k = 200) 
+radius = random.randint(3,10) 
+pi = math.pi
+
