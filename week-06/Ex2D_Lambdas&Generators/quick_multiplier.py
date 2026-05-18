5b7dee11cef9438a1e03a2d0b7a036568cf01ecd
maritzaruiz2026 <mruiz3@my.yearupunited.org>
2026-05-18 12:07:45 -0500

    Exercise 2.D Lambdas and generators LAB 1

--- week-06/Ex2D_Lambdas&Generators/quick_multiplier.py
+++ week-06/Ex2D_Lambdas&Generators/quick_multiplier.py
@@ -1,1 +1,35 @@
-doubler = lambda n: n * 2
+#doubler = lambda n: n * 2
+
+#print(doubler(8))
+#print(doubler(-4))
+#print(doubler('banana'))
+
+#tripler = lambda t: t * 3
+#print(tripler(8))
+#print(tripler(-4))
+#print(tripler('banana'))
+
+def times(n):
+    return lambda x : x * n
+
+quadrupler = times(4) 
+quintupler = times(5) 
+sextupler = times(6) 
+septupler = times(7)
+octupler = times(8)
+nonupler = times(9)
+decupler = times(10)
+
+print(quadrupler(1))
+print(quintupler(1))
+print(sextupler(1))
+print(octupler(1))
+print(nonupler(1))
+print(decupler(1))
+
+print(quadrupler(2))
+print(quintupler(2))
+print(sextupler(2))
+print(octupler(2))
+print(nonupler(2))
+print(decupler(2))
